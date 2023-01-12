b27f208e29f623a3ce381291f9e28a30232c9959
philipqueen <queen.philip@gmail.com>
2023-01-12 08:56:48 -0700

    Updates to data workflow

--- mocap_data_workflow/construct_BOS_array.py
+++ mocap_data_workflow/construct_BOS_array.py
@@ -1,13 +1,11 @@
 import numpy as np
 from pathlib import Path
 
-from session_read_in import FreemocapSession
-
 def construct_BOS_array(session_path_dict: dict, BOS_frame_list: list, starting_foot: str) -> list:
-    '''equivalent code to replicate: 
-    BOS_frame_x = np.concatenate((Lfoot_COM_frame_xyz[:3371,0], Rfoot_COM_frame_xyz[3371:4371,0], Lfoot_COM_frame_xyz[4371:,0]))'''
+    '''Constructs array for base of support depending on which foot is on the line during the given session. '''
+
     segment_com_frame_joint_xyz = np.load(session_path_dict["segment_COM_frame_xyz"])
-    
+    print(f"Shape of segment_com_frame_joint_xyz: {segment_com_frame_joint_xyz.shape}")
     com_frame_xyz_dict = {"right": segment_com_frame_joint_xyz[:,12,:],
                         "left": segment_com_frame_joint_xyz[:,13,:]}
 
@@ -20,8 +18,9 @@ def construct_BOS_array(session_path_dict: dict, BOS_frame_list: list, starting_
             BOS_frame_x = np.concatenate((BOS_frame_x, com_frame_xyz_dict[current_foot][start_frame:, 0]))
         else:
             end_frame = BOS_frame_list[index+1]
-            BOS_frame_x = np.concatenate((BOS_frame_x, com_frame_xyz_dict[current_foot][start_frame:end_frame, 0])) #why does concatenating not work???
+            BOS_frame_x = np.concatenate((BOS_frame_x, com_frame_xyz_dict[current_foot][start_frame:end_frame, 0]))
     
+    print(f"Shape of BOS_frame_X: {BOS_frame_x.shape}")
     return BOS_frame_x
 
 
@@ -29,13 +28,14 @@ def construct_BOS_array(session_path_dict: dict, BOS_frame_list: list, starting_
 def main():
     session_id = "4stepsequence_session5_10_5_22"
     freemocap_data_folder_path = Path("/Users/philipqueen/Documents/Humon Research Lab/FreeMocap_Data")
-    session_info = FreemocapSession(session_id, freemocap_data_folder_path)
-    print(session_info.path_dict["data_arrays_folder"])
+    session_folder_path = freemocap_data_folder_path / session_id
+    path_dict_file_name = "session_path_dict.npy"
+    path_dict_file_path = session_folder_path / path_dict_file_name
 
+    path_dict = np.load(path_dict_file_path, allow_pickle=True).item()
     BOS_frame_list = [0, 3371, 4371]
     starting_foot = "left"
-    BOS_frame_x = construct_BOS_array(session_info.path_dict, BOS_frame_list, starting_foot)
-    print(BOS_frame_x.shape)
+    BOS_frame_x = construct_BOS_array(path_dict, BOS_frame_list, starting_foot)
 
 
 
--- mocap_data_workflow/session_read_in.py
+++ mocap_data_workflow/session_read_in.py
@@ -1,31 +1,51 @@
+import os
+import numpy as np
 from pathlib import Path
 
-class FreemocapSession:
-    def __init__(self, session_id: str, freemocap_data_folder_path: Path) -> dict:
-        self.session_id = session_id
-        self.freemocap_data_folder_path = freemocap_data_folder_path
+from construct_BOS_array import construct_BOS_array
 
+class FreemocapSession:
+    def __init__(self, session_id: str, freemocap_data_folder_path: Path):
         # check if dictionary exists in session folder
         # if dictionary exists, load it and set it to self.path_dict
         # if dictionary doesn't exist, run construct_path_dict
+        self.construct_path_dict(session_id, freemocap_data_folder_path)
 
-    def construct_path_dict(self,):    
+
+    def construct_path_dict(self, session_id: str, freemocap_data_folder_path: Path):
         self.path_dict = {}
-        self.path_dict["session_id"] = self.session_id
-        self.path_dict["freemocap_data_folder"] = self.freemocap_data_folder_path
-        self.path_dict["session_folder"] = self.freemocap_data_folder_path / self.session_id
+        self.path_dict["session_id"] = session_id
+        self.path_dict["freemocap_data_folder"] = freemocap_data_folder_path
+        self.path_dict["session_folder"] = freemocap_data_folder_path / session_id
         self.path_dict["data_arrays_folder"] = self.path_dict["session_folder"] / "DataArrays"
         self.path_dict["synced_videos_folder"] = self.path_dict["session_folder"] / "SyncedVideos"
         self.path_dict["mediapipe_blender_rotated"] = self.path_dict["data_arrays_folder"] / "mediaPipeSkel_3d_filtered.npy"
         self.path_dict["total_body_COM_frame_xyz"] = self.path_dict["data_arrays_folder"] / "totalBodyCOM_frame_XYZ.npy"
         self.path_dict["segment_COM_frame_xyz"] = self.path_dict["data_arrays_folder"] / "segmentedCOM_frame_joint_XYZ.npy"
 
+    def save_path_dict(self):
+        path_dict_file_name = "session_path_dict.npy"
+        path_dict_file_path = self.path_dict["session_folder"] / path_dict_file_name
+        np.save(path_dict_file_path, self.path_dict)
+
+    def construct_session_info_dict(self, BOS_frame_list: list, starting_foot: str, good_a_pose_frame_number: int):
+        self.session_info_dict = {}
+        self.session_info_dict["BOS_frame_list"] = BOS_frame_list
+        self.session_info_dict["starting_foot"] = starting_foot
+        self.session_info_dict["BOS_array"] = construct_BOS_array(self.path_dict, BOS_frame_list, starting_foot)
+        self.session_info_dict["good_a_pose_frame_number"] = good_a_pose_frame_number
+
+
+
+    
+
 
 def main():
     session_id = "4stepsequence_session5_10_5_22"
     freemocap_data_folder_path = Path("/Users/philipqueen/Documents/Humon Research Lab/FreeMocap_Data")
     session_info = FreemocapSession(session_id, freemocap_data_folder_path)
-    print(type(session_info.dict["data_arrays"]))
+    session_info.save_path_dict()
+    
 
 if __name__ == "__main__":
     main()
