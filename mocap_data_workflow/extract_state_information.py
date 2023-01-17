c72abda02736cc0aa0d0deccb51d62b93d8552c1
philipqueen <queen.philip@gmail.com>
2023-01-17 14:16:13 -0700

    Add angle measurement and tests

--- mocap_data_workflow/extract_state_information.py
+++ mocap_data_workflow/extract_state_information.py
@@ -13,19 +13,36 @@ def get_state_information(BOS_trajectories_frame_xyz, com_trajectories_frame_xyz
     com_frame_x = com_trajectories_frame_xyz[:,0]
     com_velocity = np.diff(pad_array(com_frame_x))
 
+def get_virtual_pendulum_angle_array(BOS_trajectories_frame_xyz, com_trajectories_frame_xyz):
+    '''Get angle of virtual pendulum between BOS and COM (on x and z axis) for each frame'''
+    pass
+
 def get_virtual_pendulum_angle(base_coordinate_xyz, top_coordinate_xyz):
-    '''Find angle, in degrees, from vertical line x=0, between base coordinate and top coordinate. 
+    '''Find angle, in degrees, from vertical line (x-axis), between base coordinate and top coordinate. 
     Clockwise angles are positive, counter clockwise angles are negative.
     '''
     
     pendulum_line = [(base_coordinate_xyz[0],base_coordinate_xyz[2]),(top_coordinate_xyz[0],top_coordinate_xyz[2])] #vector representing the pendulum
-    vertical_line = [(base_coordinate_xyz[0],base_coordinate_xyz[2]),(base_coordinate_xyz[0],top_coordinate_xyz[2])] #vector representing the 
+    vertical_line = [(base_coordinate_xyz[0],base_coordinate_xyz[2]),(base_coordinate_xyz[0],top_coordinate_xyz[2])] #vector representing the vertical line (line of gravity)
 
+    # normalizing vectors is best practice
+    normalized_pendulum_vector = normalize_vector(pendulum_line)
+    normalized_vertical_vector = normalize_vector(vertical_line)
 
+    angle_between_vectors_radians = get_angle_between_vectors(normalized_vertical_vector, normalized_pendulum_vector)
+    return np.degrees(angle_between_vectors_radians)
 
-def get_virtual_pendulum_angle_array(BOS_trajectories_frame_xyz, com_trajectories_frame_xyz):
-    '''Get angle of virtual pendulum between BOS and COM (on x and z axis) for each frame'''
-    pass
+def get_angle_between_vectors(reference_vector, signed_vector):
+    '''Gives the signed angle in radians between two normalized vectors. 
+    The sign of the angle will be positive if the signed vector is clockwise of the reference vector, and negative otherwise.
+    '''
+
+    return np.arctan2(np.cross(signed_vector,reference_vector),np.dot(reference_vector,signed_vector))
+
+def normalize_vector(vector):
+    '''Create vector with length 1 in same direction as input vector (equivalent to unit vector)'''
+
+    return vector / np.linalg.norm(vector)
 
 def pad_array(array):
     '''Duplicates the first item of an array to preserve array size while differentiating.'''
