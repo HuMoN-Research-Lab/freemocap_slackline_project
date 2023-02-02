92d26587b6cc3e19621361c775cc2ea957238d06
philipqueen <queen.philip@gmail.com>
2023-02-01 18:25:23 -0700

    Fill in array length checker

--- mocap_data_workflow/extract_state_information.py
+++ mocap_data_workflow/extract_state_information.py
@@ -19,7 +19,7 @@ def get_state_information(BOS_trajectories_frame_xyz, com_trajectories_frame_xyz
     logging.debug(f"Shape of pendulum angle array is: {pendulum_angle_frame.shape}")
     pendulum_velocity = np.diff(pad_array(pendulum_angle_frame))
 
-    if check_arrays_have_same_length([BOS_frame_x, BOS_velocity, com_frame_x, com_velocity, pendulum_angle_frame, pendulum_velocity]):
+    if check_arrays_have_same_shape([BOS_frame_x, BOS_velocity, com_frame_x, com_velocity, pendulum_angle_frame, pendulum_velocity]):
         state_information = []
         for frame in range(BOS_frame_x.shape[0]):
             this_frame_state = np.array([BOS_frame_x[frame], BOS_velocity[frame], com_frame_x[frame], com_velocity[frame], pendulum_angle_frame[frame], pendulum_velocity[frame]])
@@ -73,8 +73,27 @@ def normalize_vector(vector):
     return vector / np.linalg.norm(vector)
 
 
-def check_arrays_have_same_length(list_of_arrays: list) -> bool:
-    '''Checks if all arrays in provided list have the same length.'''
+def check_arrays_have_same_shape(list_of_arrays: list) -> bool:
+    '''Check if all arrays in provided list have the same length.'''
+    if check_arrays_are_ndarrays(list_of_arrays):
+        shape_to_check_against = list_of_arrays[0].shape
+
+        list_of_shapes = [array.shape for array in list_of_arrays if array.shape != shape_to_check_against]
+        if len(list_of_shapes) == 0:
+            logging.debug("All input arrays are the same length")
+            return True
+    
+        logging.error("Arrays are not all of same length")
+        return False
+
+def check_arrays_are_ndarrays(list_of_potential_arrays: list) -> bool:
+    '''Check if all arrays in provided list are of the type np.ndarray, to catch errors before calling .shape'''
+    for array in list_of_potential_arrays:
+        if not isinstance(array, np.ndarray):
+            logging.error(f"{array} must be of type np.ndarray")
+            return False
+    
+    logging.debug("All arrays are np.ndarray type")
     return True
 
 
