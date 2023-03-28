80e3f841be53c715123973d1190324a6fc039568
philipqueen <queen.philip@gmail.com>
2023-03-28 09:40:26 -0600

    add foot plots

---
+++ docs/Slackline Paper Outline Draft.md
@@ -0,0 +1,37 @@
+# Slackline Outline Draft
+
+## Introduction
+
+A comprehensive literature review can be found at: [slackline lit review](https://humon-research-lab.github.io/freemocap_slackline_project/lit_review_notes/Slackline%20Literature%20Survey/). The relevant sections are 'Slackline Control Theory', 'Slacklining Balance Strategies', and 'Slacklining Performance Indicators and Measures of Stability'.
+
+## Results 
+
+![Left toot standing video screenshot](https://i.imgur.com/kFehZn2.jpg)
+- Screenshot from left foot standing session showing 2d motion capture overlay, 3d motion capture animation, distance of center of mass from base of support in the mediolateral direction, and the state space (distance vs. velocity) of the mediolateral distance between the center of mass and base of support.
+
+![Left foot standing time series](https://i.imgur.com/dXkyLmF.png)
+- Time series of four sessions of left foot standing, each showing the mediolateral distance between the center of mass and base of support.
+
+![Left foot standing state space](https://i.imgur.com/UNHYSVN.png)
+- The state space (distance vs. veclocity) of the mediolateral distance between the center of mass and base of support for 12 seconds of left foot standing
+
+![Virtual pendulum overlay on motion capture data](https://i.imgur.com/I7xBFPJ.png)
+- Overlay of the virtual pendulum from the base of support to the center of mass, displayed over animation of motion capture data.
+
+![[LeftFootStandingX.png]]
+- Right vs left hand motions in the mediolateral direction. Investigating the claim from [[Performance indicators for stability of slackline balancing]] that hand coordination is an indicator of slackline expertise.
+![[LeftFootStandingZ.png]]
+- Right vs left hand motions in the vertical direction. Investigating the claim from [[Performance indicators for stability of slackline balancing]] that hand coordination is an indicator of slackline expertise.
+![[LeftFootStanding.png]]
+-Foot motion patterns on the mediolateral plane, investigating the modeling from [[Balancing on tightropes and slacklines - Paoletti Mahadevan 2012]] and [[An LMI Approach to Controller Design for Balancing over Slackline - Iqbal 2019]] that model the "slackliners" foot as traveling along a circular track rather than on a springed horizontal track.
+
+
+## Materials and Methods
+
+### Slackline Protocol
+
+We recorded motion capture of X skilled slackliners performing the same balance protocol while standing on a slackline. The balance protocol involved X seconds of single leg standing on the right foot, X seconds of single leg standeng on the right foot, and X seconds of tandem stance standing with the subject's preferred foot forward. The slackline used was X meters long, had Y kN of tension on it, and the webbing was a flat weave made of polyester. 
+
+### Motion Capture
+
+We recorded the motion capture using Freemocap's markerless motion capture software, with 2D pose estimation performed by Google's Mediapipe Holistic model. We used four GoPro Hero 7 Black cameras to record the video in 2.7K at a framerate of 60Hz. The recordings were made outside with the subjects wearing normal street clothes. 
---
+++ mocap_data_workflow/plotting/foot_movement_path/foot_path_plots.py
@@ -0,0 +1,130 @@
+from statistics import mean, stdev
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+from matplotlib.animation import FuncAnimation
+import cv2
+from pathlib import Path
+
+class LeftFootPlots:
+    def __init__(self):
+        pass
+
+    def create_plots(self):
+        # load in all of the data
+        # set session folder path
+        self.freemocap_data_path = Path("/Users/philipqueen/Documents/Humon Research Lab/FreeMocap_Data")
+        self.session2_id = "4stepsequence_session2_10_5_22"
+        self.session3_id = "4stepsequence_session3_10_5_22"
+        self.session4_id = "4stepsequence_session4_10_5_22"
+        self.session5_id = "4stepsequence_session5_10_5_22"
+
+        self.session3_info_dict = self.load_data(self.freemocap_data_path, self.session3_id)
+        self.session4_info_dict = self.load_data(self.freemocap_data_path, self.session4_id)
+        self.session2_info_dict = self.load_data(self.freemocap_data_path, self.session2_id)
+        self.session5_info_dict = self.load_data(self.freemocap_data_path, self.session5_id)
+
+        #parameters:
+        self.fps = 60
+        self.length_of_data_seconds = 15
+        self.length_of_data_frames = self.length_of_data_seconds * self.fps
+        self.initial_offset_seconds = 0
+        self.initial_offset_frames = self.initial_offset_seconds * self.fps
+
+
+        self.initialize_figure()
+        self.create_subplot1(self.session2_info_dict)
+        self.create_subplot2(self.session3_info_dict)
+        self.create_subplot3(self.session4_info_dict)
+        self.create_subplot4(self.session5_info_dict)
+
+
+    def load_data(self, freemocap_data_path, session):
+        session_folder_path = freemocap_data_path / session
+        info_dict_file_path = session_folder_path / "session_info_dict.npy"
+
+        # load base of support data based on which foot I'm standing on
+        if session == "4stepsequence_session2_10_5_22":
+            start_frame = 2400
+            info_dict = np.load(info_dict_file_path, allow_pickle=True).item()
+        elif session == "4stepsequence_session3_10_5_22":
+            start_frame = 3900
+            info_dict = np.load(info_dict_file_path, allow_pickle=True).item()
+        elif session == "4stepsequence_session4_10_5_22":
+            start_frame = 3450
+            info_dict = np.load(info_dict_file_path, allow_pickle=True).item()
+        elif session == "4stepsequence_session5_10_5_22":
+            start_frame = 3540
+            info_dict = np.load(info_dict_file_path, allow_pickle=True).item()
+
+        info_dict["start_frame"] = start_frame
+
+        return info_dict
+
+    def initialize_figure(self):
+        self.fig = plt.figure(constrained_layout=True)
+        self.fig.set_size_inches(7,8)
+        #self.fig.set_dpi(300)
+        self.spec = self.fig.add_gridspec(2, 2) # gridspec allows us to have one plot span multiple plot grids
+
+        self.fig.suptitle(f'Foot Motion Patterns \nFor Left Foot Standing on a Slackline for {self.length_of_data_seconds} Seconds')
+
+    def create_subplot1(self, session_data_dict):
+        self.ax1 = self.fig.add_subplot(self.spec[0, 0]) 
+
+        self.ax1.set_title("Session 2 (Subject 2)")
+        self.ax1.set_xlabel(f"Base of Support X [mm]")
+        self.ax1.set_ylabel(f"Base of Support Z [mm]")
+
+        start_frame = session_data_dict["start_frame"] + self.initial_offset_frames
+        end_frame = start_frame + self.length_of_data_frames
+
+        self.plot_state_space_shadow, = self.ax1.plot(session_data_dict["BOS_frame_xyz"][start_frame:end_frame, 0], session_data_dict["BOS_frame_xyz"][start_frame:end_frame, 2], zorder=1, color="plum")
+
+
+    def create_subplot2(self, session_data_dict):
+        self.ax2 = self.fig.add_subplot(self.spec[0, 1]) 
+
+        self.ax2.set_title("Session 3 (Subject 3)")
+        self.ax2.set_xlabel(f"Base of Support X [mm]")
+        self.ax2.set_ylabel(f"Base of Support Z [mm]")
+
+        start_frame = session_data_dict["start_frame"] + self.initial_offset_frames
+        end_frame = start_frame + self.length_of_data_frames
+
+        self.plot_state_space_shadow, = self.ax2.plot(session_data_dict["BOS_frame_xyz"][start_frame:end_frame, 0], session_data_dict["BOS_frame_xyz"][start_frame:end_frame, 2], zorder=1, color="plum")
+
+    def create_subplot3(self, session_data_dict):
+        self.ax3 = self.fig.add_subplot(self.spec[1, 0]) 
+
+        self.ax3.set_title("Session 4 (subject 3)")
+        self.ax3.set_xlabel(f"Base of Support X [mm]")
+        self.ax3.set_ylabel(f"Base of Support Z [mm]")
+
+        start_frame = session_data_dict["start_frame"] + self.initial_offset_frames
+        end_frame = start_frame + self.length_of_data_frames
+
+        self.plot_state_space_shadow, = self.ax3.plot(session_data_dict["BOS_frame_xyz"][start_frame:end_frame, 0], session_data_dict["BOS_frame_xyz"][start_frame:end_frame, 2], zorder=1, color="plum")
+
+    def create_subplot4(self, session_data_dict):
+        self.ax4 = self.fig.add_subplot(self.spec[1, 1])
+
+        self.ax4.set_title("Session 5 (Subject 4)")
+        self.ax4.set_xlabel(f"Base of Support X [mm]")
+        self.ax4.set_ylabel(f"Base of Support Z [mm]")
+
+        start_frame = session_data_dict["start_frame"] + self.initial_offset_frames
+        end_frame = start_frame + self.length_of_data_frames
+
+        self.plot_state_space_shadow, = self.ax4.plot(session_data_dict["BOS_frame_xyz"][start_frame:end_frame, 0], session_data_dict["BOS_frame_xyz"][start_frame:end_frame, 2], zorder=1, color="plum")
+ 
+
+
+def main():
+    slack_plots = LeftFootPlots()
+    slack_plots.create_plots()
+    plt.show()
+
+
+if __name__ == "__main__":
+    main()
